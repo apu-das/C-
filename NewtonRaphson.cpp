1c23ea086788089d4fe03f8a34766fe532b9ff08
Apu Das <74287939+apu-das@users.noreply.github.com>
2023-11-30 00:23:18 +0600

    Add files via upload

---
+++ NewtonRaphson.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define EPSILON 0.001
+using namespace std;
+
+double func(double x)
+{
+    return x*x*x -x*x+2;
+}
+double derivfunc(double x)
+{
+    return 3*x*x - 2*x;
+}
+
+void newtonraphson(double x)
+{
+    double h = func(x)/derivfunc(x);
+    while (abs(h)>=EPSILON)
+    {
+        h= func(x)/derivfunc(x);
+        x=x-h;
+    }
+    cout<<"The values of the root is: "<<x;
+}
+
+int main()
+{
+    double x0=-20;
+    newtonraphson(x0);
+    return 0;
+}
